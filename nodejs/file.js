86543f882c71c902a0bc21e76699228f341c582e
TANISHA BANSAL <tanishab130@gmail.com>
2024-09-23 19:33:27 +0530

    basic

--- nodejs/file.js
+++ nodejs/file.js
@@ -1,7 +1,25 @@
 const fs=require("fs") //built in
 
 // sync call
-fs.writeFileSync('./test.txt', "Hey There");
+//fs.writeFileSync('./test.txt', "Hey There");
 
 //async
-fs.writeFile("./test.txt", "Hey There Async",(err)=>{})
+//fs.writeFile("./test.txt", "Hey There Async",(err)=>{})
+
+//const result=fs.readFileSync('./contacts.txt', "utf-8");
+//console.log(result);  //cant read 2 text
+
+//async dont return anything  inspect callback
+// fs.readFile("./contacts.txt","utf-8", (err, result)=>{
+//     if(err) console.log("Error",err);
+//     else
+//     console.log(result);
+// })
+
+//fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+
+fs.appendFileSync("./test.txt",`${Date.now()} Hey There\n`);
+
+fs.cpSync('./test.txt', './copy.txt'); //copying file
+fs.unlinkSync("copy.txt");  //delete file
+console.log(fs.statSync("./test.txt"));
